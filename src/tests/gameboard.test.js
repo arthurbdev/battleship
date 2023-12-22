c00415c1309f5edfab66c75b4a6b03b2b75a88b8
Arthur Bais <artb@tutamail.com>
2023-12-22 22:07:34 +0200

    Add more tests for x and y placement for ships

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -21,4 +21,39 @@ test("Place ship horizontal", () => {
   expect(gameboard.placeShip(3, 4, 4)).toBeNull();
   expect(gameboard.placeShip(3, 8, 4)).toBeNull();
   expect(gameboard.placeShip(3, 8, 4)).toBeNull();
+
+  // if ships on border corner
+  expect(gameboard.placeShip(4, 3, 1)).toBeNull();
+  expect(gameboard.placeShip(2, 3, 1)).toBeNull();
+  expect(gameboard.placeShip(2, 8, 1)).toBeNull();
+  expect(gameboard.placeShip(4, 8, 1)).toBeNull();
+
+  // place ship in corner
+  expect(gameboard.placeShip(9, 0, 3)).not.toBeNull();
+  expect(gameboard.placeShip(0, 0, 3)).not.toBeNull();
+  expect(gameboard.placeShip(0, 7, 3)).not.toBeNull();
+  expect(gameboard.placeShip(9, 7, 3)).not.toBeNull();
+});
+
+test("Place ship vertical", () => {
+  const gameboard = new Gameboard();
+  const sh = gameboard.placeShip(3, 4, 4, false);
+  expect(gameboard.board[3][4].ship).toBe(sh);
+  expect(gameboard.board[4][4].ship).toBe(sh);
+  expect(gameboard.board[5][4].ship).toBe(sh);
+  expect(gameboard.board[6][4].ship).toBe(sh);
+  expect(gameboard.placeShip(2, 4, 4, false)).toBeNull();
+  expect(gameboard.placeShip(1, 5, 2)).toBeInstanceOf(Ship);
+
+  // if ships on border corner
+  expect(gameboard.placeShip(2, 3, 1)).toBeNull();
+  expect(gameboard.placeShip(2, 5, 1)).toBeNull();
+  expect(gameboard.placeShip(7, 3, 1)).toBeNull();
+  expect(gameboard.placeShip(7, 5, 1)).toBeNull();
+
+  // place ship in corner
+  expect(gameboard.placeShip(7, 9, 3, false)).not.toBeNull();
+  expect(gameboard.placeShip(0, 9, 3, false)).not.toBeNull();
+  expect(gameboard.placeShip(7, 0, 3, false)).not.toBeNull();
+  expect(gameboard.placeShip(0, 0, 3, false)).not.toBeNull();
 });
