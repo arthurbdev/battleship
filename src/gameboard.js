4a51f6cf7ed19c1410ba4f39048c281d6de1b449
Arthur Bais <artb@tutamail.com>
2023-12-22 18:11:04 +0200

    Add placeship method(only horizontal)

--- src/gameboard.js
+++ src/gameboard.js
@@ -16,6 +16,32 @@ class Gameboard {
     }
   };
 
+  placeShip = (y, x, len, h = true) => {
+    if (this.validatePlacement(y, x, len)) {
+      const ship = new Ship(len);
+      this.ships.push(ship);
+      for (let i = 0; i < len; i++) {
+        this.board[y][x + i].ship = ship;
+      }
+      return ship;
+    }
+    return null;
+  };
+
+  validatePlacement = (y, x, len) => {
+    const b = this.board;
+    const bool =
+      b[y].slice(x, x + len).every(
+        (cell, i) =>
+          cell && // cell exists
+          !cell.ship && // no ship on cell
+          !b[y - 1][x + i].ship && // no ship above or below
+          !b[y + 1][x + i].ship,
+      ) &&
+      !b[y][x - 1].ship && // no ship to the left or to the right
+      !b[y][x + len + 1].ship;
+    return bool;
+  };
 }
 
 export default Gameboard;
