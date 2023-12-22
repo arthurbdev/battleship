e976e2f6a875a3b216fc4ac72d1d75b11a40eebe
Arthur Bais <artb@tutamail.com>
2023-12-22 18:07:45 +0200

    Add ship horizontally tests

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,5 +1,6 @@
 import { test, expect } from "vitest";
 import Gameboard from "../gameboard";
+import Ship from "../ship";
 
 test("Gameboard init", () => {
   const gameboard = new Gameboard();
@@ -7,3 +8,17 @@ test("Gameboard init", () => {
   expect(gameboard.board[0].length).toBe(10);
 });
 
+test("Place ship horizontal", () => {
+  const gameboard = new Gameboard();
+  gameboard.placeShip(3, 4, 4);
+  expect(gameboard.board[3][4].ship).toBeInstanceOf(Ship);
+  expect(gameboard.board[3][4].ship.length).toBe(4);
+  expect(gameboard.board[3][5].ship).toMatchObject(gameboard.board[3][4].ship);
+  expect(gameboard.board[3][6].ship).toMatchObject(gameboard.board[3][4].ship);
+  expect(gameboard.board[3][7].ship).toMatchObject(gameboard.board[3][4].ship);
+  expect(gameboard.ships[0]).toMatchObject(gameboard.board[3][4].ship);
+  expect(gameboard.board[3][8].ship).toBeNull();
+  expect(gameboard.placeShip(3, 4, 4)).toBeNull();
+  expect(gameboard.placeShip(3, 8, 4)).toBeNull();
+  expect(gameboard.placeShip(3, 8, 4)).toBeNull();
+});
