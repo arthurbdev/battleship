310818cd86ab7ca684271dc11ec50385a021fd0e
Arthur Bais <artb@tutamail.com>
2023-12-23 22:57:44 +0200

    Game is now playable in node console

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-console.log("test.");
+import Gameboard from "./gameboard";
+import Player from "./player";
+import Game from "./game";
+
+const FLEET = {
+  // length: num of ships
+  2: 1,
+  3: 2,
+  4: 1,
+  5: 2,
+};
+
+const player = new Player("Me");
+player.placeFleet(FLEET);
+
+const g = new Game();
+const w = await g.newGame(player, FLEET);
+console.log(`Winner is ${w.name}`);
