8a96d62b14287414fe68230fc372806a42fd7caf
Arthur Bais <artb@tutamail.com>
2023-12-23 22:24:47 +0200

    Add randomAttack method for AI

--- src/player.js
+++ src/player.js
@@ -8,6 +8,17 @@ class Player {
 
   attack = (y, x, target) => target.receiveAttack(y, x);
 
+  randAttack = (target) => {
+    const maxW = target.board.length;
+    let res = null;
+    while (res === null) {
+      const x = Math.floor(Math.random() * maxW);
+      const y = Math.floor(Math.random() * maxW);
+      res = target.receiveAttack(y, x);
+    }
+    return res;
+  };
+
 }
 
 export default Player;
